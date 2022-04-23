486c01b92f43f5f9e744d024a4e77881dbd36095
Janath <250729@studenti.unimore.it>
2022-04-23 20:16:27 +0200

    aggiunti i commenti

--- code/LoggerPDS/dbmstesting/parsing/Reader.java
+++ code/LoggerPDS/dbmstesting/parsing/Reader.java
@@ -31,11 +31,21 @@ public class Reader {
             // risultato nullo
             String[] logcotto2 = lograw.split("\\s");
             // [144.86.173.78, -, -, [10/Sep/2021:21:52:51, +0000], "GET, /, HTTP/1.1", 200, 211968, "-", "Expanse,, a, Palo, Alto, Networks, company,, searches, across, the, global, IPv4, space, multiple, times, per, day, to, identify, customers&#39;, presences, on, the, Internet., If, you, would, like, to, be, excluded, from, our, scans,, please, send, IP, addresses/domains, to:, scaninfo@paloaltonetworks.com"]
+            // si potrebbe iterare la lista e unire tutti i token da sinistra a destra con questa regola :
+            //   se inizia con ' " '   appendi ed elimina tutto quello che é sucessivo finché non arrivi a qualcosa che finisce per ' " '
+            //    qui in questo esempio qua sopra partirebbe da  TOKEN[4] = "GET,
+            //      fino ad arrivare al TOKEN[6] = HTTP/1.1"  e lo unirebbe tutto in una cosa sola
+            //
+            //
             String[] logcotto3 = lograw.split("\\s");
 
             System.out.println("provo a sparare i dati");
+            System.out.println("inzio con logcotto1");
+            System.out.println("inzio con logcotto2");
             System.out.println(Arrays.toString(logcotto));
             System.out.println(Arrays.toString(logcotto2));
+
+            
                                             }
 
         }while(br.ready());
