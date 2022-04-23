da265b22637109cbf80f9ffa6b86fe66732409d0
Janath <250729@studenti.unimore.it>
2022-04-24 00:41:17 +0200

    aggiunto metodo costruttore che prende un pattern , WorkInProgress

--- code/LoggerPDS/dbmstesting/parsing/Pattern.java
+++ code/LoggerPDS/dbmstesting/parsing/Pattern.java
@@ -1,5 +1,5 @@
 package parsing;
-
+import java.util.*; // import everything including List and ArrayList
 public class Pattern {
     /*
     classe pensata per il futuro immaginatevi che qui poi dire in che ordine sono i dati cosi 
@@ -13,10 +13,71 @@ public class Pattern {
 
     }
 
+    
     public void setpattern( String nuovopattern){ this.pattern = nuovopattern ; }
     public void getpattern( String nuovopattern){ this.pattern = nuovopattern ; }
 
-    
+    /**
+     * funzione che prende in ingresso 2 variabili
+     * @param s  stringa appena estratta
+     * @param pat il pattern de server di apache 
+     * @return stringa formatata pronta al classaggio
+     */
+    public String spattern(String s , Pattern pat){
+        String formattata = "sad";
+
+        String[] decomposer = pat.pattern.split(":");
+
+        /* logica di estrazione del log dalla linea obbiettivo eliinare gli spazi e separare tutti gli argomenti in 9 blocchi
+            soluzione facile separare per spazi
+                problema : non so come evitare gli spazi dentro i apici quindi iterereo la lista basandomi su questi 2 casi
+
+        */
+        
+        List<String> list = new ArrayList<String>();
+        
+
+       
+        // convert string to `char[]` array
+        char[] chars = s.toCharArray();
+        
+ 
+        // IN-SVILUPPO NON ANCORA TESTATO
+        // praticamente itero la stringa e separo tutte le cose 
+        // esculdendo spazi e tenendo conto che tra 2 apici " ci possono essere tanti spazi"
+        int rosso = 0;
+        boolean trovato = true ; 
+        for (int blu = 0 ; blu < chars.length; ++blu) {
+            if (chars[blu]==' '){ 
+                list.add(s.substring(rosso,blu-1));
+                rosso = blu + 1; // sposto il cursore rosso in avanti 
+            }
+
+            if( chars[blu]=='"' && trovato == true ){
+                trovato = false;
+             }
+
+             if( chars[blu]=='"' && trovato == false){ 
+                trovato = true;
+                list.add(s.substring(rosso,blu-1));
+             }
+
+        }
+        
+
+        for (String i : decomposer){
+
+            switch(i) {
+                case "1":
+
+            }
+
+
+        }
+       
+
+        return formattata;
+    }
        
 
 
--- code/LoggerPDS/dbmstesting/parsing/Reader.java
+++ code/LoggerPDS/dbmstesting/parsing/Reader.java
@@ -4,7 +4,10 @@ import java.util.Arrays;
 import java.io.*;
 
 public class Reader {
-    
+    /**
+     * Leggi da file senza sapere il pattern va sviluppata della logica per riconoscere in modo automatico il contenuto delle stringhe
+     * penso che non sia neccessario sviluppare questa funzione in futuro in quanto e una funzionalitá troppo complicata
+     */
     public void leggidafile(){
     try{
         File prova = new File(""); 
@@ -30,13 +33,7 @@ public class Reader {
             String[] logcotto = lograw.split("^([\\d.]+) (\\S+) (\\S+) \\[([\\w:\\/]+\\s[+-]\\d{4})\\] \"(.+?)\" (\\d{3}) (\\d+|-) \"([^\"]+)\" \"([^\"]+)\"\\W*");
             // risultato nullo
             String[] logcotto2 = lograw.split("\\s");
-            // [144.86.173.78, -, -, [10/Sep/2021:21:52:51, +0000], "GET, /, HTTP/1.1", 200, 211968, "-", "Expanse,, a, Palo, Alto, Networks, company,, searches, across, the, global, IPv4, space, multiple, times, per, day, to, identify, customers&#39;, presences, on, the, Internet., If, you, would, like, to, be, excluded, from, our, scans,, please, send, IP, addresses/domains, to:, scaninfo@paloaltonetworks.com"]
-            // si potrebbe iterare la lista e unire tutti i token da sinistra a destra con questa regola :
-            //   se inizia con ' " '   appendi ed elimina tutto quello che é sucessivo finché non arrivi a qualcosa che finisce per ' " '
-            //    qui in questo esempio qua sopra partirebbe da  TOKEN[4] = "GET,
-            //      fino ad arrivare al TOKEN[6] = HTTP/1.1"  e lo unirebbe tutto in una cosa sola
-            //
-            //
+            
             String[] logcotto3 = lograw.split("\\s");
 
             System.out.println("provo a sparare i dati");
@@ -45,7 +42,7 @@ public class Reader {
             System.out.println(Arrays.toString(logcotto));
             System.out.println(Arrays.toString(logcotto2));
 
-            
+
                                             }
 
         }while(br.ready());
@@ -60,10 +57,50 @@ public class Reader {
 
 }
 
+/**
+ * leggi da file conoscendo il pattern quindi assegnando direttamente le varie cose
+ * @param a
+ */
+public void leggidafile(Pattern a){
 
-/*
+        // Conosco il pattern e quindi so gia cosa aspettarmi in quale posizione 
 
 
-*/
+    try{
+        File prova = new File(""); 
+        
+        // C:\Users\Janat\Desktop\pds\2022t4\code\LoggerPDS\dbmstesting\parsing\logsexample\gnetshop.log.1
+        File file = new File(prova.getAbsolutePath()+File.separator+"parsing"+File.separator+"logsexample"+File.separator+"gnetshop.log.1");
+        
 
+        FileReader fin= new FileReader(file);
+
+        BufferedReader br =new BufferedReader(fin);
+        do{
+           
+            String lograw = br.readLine();
+            
+           
+            
+            System.out.println(lograw);
+
+            if(!lograw.equals(null)){
+
+                //METODO ANCORA IN SVILUPPO , NON ANCORA TESTATO
+                a.spattern(lograw,a);
+
+
+             }
+
+        }while(br.ready());
+        
+
+        fin.close();
+    }catch(IOException e) {
+        System.out.println("Apertura file fallita");
+        e.printStackTrace();
+        System.exit(-1);
+    }
+
+}
 }
