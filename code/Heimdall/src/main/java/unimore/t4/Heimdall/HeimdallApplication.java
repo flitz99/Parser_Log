6656d9c1eeebe6cb8a7d1dc2e2b1c3e052e9186d
Bitlorenz <lollolucchi98@gmail.com>
2022-04-27 16:04:14 +0200

    sistemazione spring project

---
+++ code/Heimdall/src/main/java/unimore/t4/Heimdall/HeimdallApplication.java
@@ -0,0 +1,17 @@
+package unimore.t4.Heimdall;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class HeimdallApplication {
+        public HeimdallApplication() {
+        }
+
+        public static void main(String[] args) {
+            SpringApplication.run(HeimdallApplication.class, args);
+        }
+    }
+
