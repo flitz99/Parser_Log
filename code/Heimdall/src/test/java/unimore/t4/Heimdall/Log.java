66f28bf0acb7f5c13309c78a0dcaa2bf948f1c3e
Janath <250729@studenti.unimore.it>
2022-04-28 09:28:54 +0200

    creati scheletri delle classi MYsql

---
+++ code/Heimdall/src/test/java/unimore/t4/Heimdall/Log.java
@@ -0,0 +1,73 @@
+package unimore.t4.Heimdall;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Log implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false,updatable = false)
+    private Long idLog;
+
+    private String Data;
+    private String Time;
+    private String Ident;
+    private String Auth;
+    private String Ipclient;
+    private String Request;
+    private Integer Response;
+    private Integer Bytes;
+    private String Agent;
+
+    private String LogOriginal;
+
+
+
+    public void setId(Long idLog) {
+        this.idLog = idLog;
+    }
+
+    public void setData(String data) {
+        Data = data;
+    }
+
+    public void setTime(String time) {
+        Time = time;
+    }
+
+    public void setIdent(String ident) {
+        Ident = ident;
+    }
+
+    public void setAuth(String auth) {
+        Auth = auth;
+    }
+
+    public void setIpclient(String ipclient) {
+        Ipclient = ipclient;
+    }
+
+    public void setRequest(String request) {
+        Request = request;
+    }
+
+    public void setResponse(Integer response) {
+        Response = response;
+    }
+
+    public void setBytes(Integer bytes) {
+        Bytes = bytes;
+    }
+
+    public void setAgent(String agent) {
+        Agent = agent;
+    }
+
+
+
+
+
+
+
+}
