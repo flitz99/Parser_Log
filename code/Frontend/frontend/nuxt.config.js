303ac87c8daaea0fd7acb66fa09966e2f262bee5
massimiliano <256091@studenti.unimore.it>
2022-05-04 14:15:04 +0200

    Set up main font from Google Font

--- code/Frontend/frontend/nuxt.config.js
+++ code/Frontend/frontend/nuxt.config.js
@@ -8,11 +8,18 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { 
+	hid: 'description', 
+	name: 'description',
+	content: '2022t4 frontend'
+      },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stykesheet', href: 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600;700&display=swap' },
     ]
   },
 
