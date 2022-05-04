4907baa2b97aebb49d4b3922abfc71ed3d64a90f
massimiliano <256091@studenti.unimore.it>
2022-05-04 20:12:10 +0200

    Login page created

--- code/Frontend/frontend/pages/index.vue
+++ code/Frontend/frontend/pages/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <Tutorial/>
+
+  <b-container class="main-cont">
+    
+    <h1 class="f-sm-b">Heimdall</h1>
+
+    <b-row>
+
+      <b-col>
+
+        <p class="f-reg mt-2">
+          Benvenuto sul sito Heimdall 
+          del gruppo 4.
+        </p>
+
+      </b-col>
+
+      <b-col>
+
+        <Login />
+      
+      </b-col>
+
+    </b-row> 
+
+  </b-container>
+
 </template>
 
 <script>
@@ -7,3 +32,12 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+
+<style>
+
+  .main-cont { margin-top: 180px;  }
+  h3 {letter-spacing: -0.3px;}
+  p { color: var(--c-grey);}
+  
+</style>
