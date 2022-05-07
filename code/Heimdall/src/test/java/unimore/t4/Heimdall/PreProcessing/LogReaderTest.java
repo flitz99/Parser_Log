cc4c7492d4779fb61585c7f56dfa37c7ac07cb1b
Jacopo-Vittorio <43786516+Jacopo-Vittorio@users.noreply.github.com>
2022-05-07 19:12:46 +0200

    inizio implementazioni del testing

---
+++ code/Heimdall/src/test/java/unimore/t4/Heimdall/PreProcessing/LogReaderTest.java
@@ -0,0 +1,12 @@
+package unimore.t4.Heimdall.PreProcessing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogReaderTest {
+
+    @Test
+    void readAllLogFile() {
+    }
+}
