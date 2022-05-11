83096632c9149bb55d6255e1d0316a97f5108822
Jacopo-Vittorio <43786516+Jacopo-Vittorio@users.noreply.github.com>
2022-05-11 15:38:13 +0200

    testing nullo

---
+++ code/Heimdall/src/test/java/unimore/t4/Heimdall/PreProcessing/JsonWriterTest.java
@@ -0,0 +1,13 @@
+package unimore.t4.Heimdall.PreProcessing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonWriterTest {
+
+    @Test
+    void writeOnJson() {
+
+    }
+}
