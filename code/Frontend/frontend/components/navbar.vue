d4d0d55e89267df0512ca77b40308ee576a1eb33
massimiliano <maxvilla970@gmail.com>
2022-05-22 17:06:36 +0200

    Removed search component

--- code/Frontend/frontend/components/navbar.vue
+++ code/Frontend/frontend/components/navbar.vue
@@ -3,10 +3,6 @@
         <nav>
             <b-container class="navbar">
                 <p class="brand m-3">H</p>
-                <b-nav-form class="ml-5">
-                    <b-form-input size="sm" class="mr-sm-2" placeholder="Cerca nel log.."></b-form-input>
-                    <b-button size="sm" class="my-2 my-sm-0 butn" type="submit">Search</b-button>
-                </b-nav-form>
             </b-container>  
         </nav>   
     
