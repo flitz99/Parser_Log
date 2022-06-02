2ce795fd2f70c076b48deab4a07fa264a37b0ca6
massimiliano <maxvilla970@gmail.com>
2022-06-02 15:20:40 +0200

    Add HTTP Stutus Code reference

---
+++ code/Frontend/frontend/pages/info.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <b-container class="info p-0 text-center shadow p-3 mb-5 bg-white rounded">
+     <img src="~/assets/status.jpg" class="img-status p-5"/>
+  </b-container>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+    .info{
+        margin-top: 20vh;
+    }
+    .img-status{
+      /*height: 900px;*/
+    }
+</style>
